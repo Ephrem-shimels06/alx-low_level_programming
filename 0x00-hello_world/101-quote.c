0382b9a9a32d2430ebaca1c78ff3b51e450867be
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-02-26 13:57:33 -0800

    generates assembly code in intel syntax

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * It prints the sentence stored at a variable strr
+ *
+ * retruns 1
+ * /
+ 
+int main(void)
+{
+  char* strr = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+  int i = 0;
+  while( i < 58)
+  {
+    putchar(strr[i]);
+    i++;
+  }
+  return (1); 
+}
