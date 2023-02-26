a429484c7718cd8123cb5c428f14b51663b50086
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-02-26 13:45:09 -0800

    prints a sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+
 int main()
+
 {
-    char* test = "\"Programming is like building a multilingual puzzle\n";
+    char* test = "\"Programming is like building a multilingual puzzle";
     puts(test);
-    return 0;
+    return (0);
 }
