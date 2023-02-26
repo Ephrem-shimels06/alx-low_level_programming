8bfc77852eced1e4d68dac6de64bc9479f736228
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-02-26 13:50:35 -0800

    prints a sentence and size of data types in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(void)
+{
+    
+    printf("Size of a char: ");
+    printf("%d",sizeof(char));
+    printf(" byte(s)\n");
+
+    printf("Size of an int: ");
+    printf("%d",sizeof(int));
+    printf(" byte(s)\n");
+
+    printf("Size of a long int: ");
+    printf("%d",sizeof(long int));
+    printf(" byte(s)\n");
+
+    printf("Size of a long long int: ");
+    printf("%d",sizeof(long long int));
+    printf(" byte(s)\n");
+
+    printf("Size of a float: ");
+    printf("%d",sizeof(float));
+    printf(" byte(s)\n");
+    return 0;
+}
