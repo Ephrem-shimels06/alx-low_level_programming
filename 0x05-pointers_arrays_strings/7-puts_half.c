9d1d1c4ac4585eb755719157756b0b7919cd6fcc
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-02-28 14:47:09 -0800

    modify 7-puts_hlaf.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,11 +11,11 @@
 
 void puts_half(char *str)
 {
-	int i = strlen(str) / 2;
+        long unsigned i = strlen(str) / 2;
 	long unsigned j = (strlen(str) - 1) / 2 ;
 	if (strlen(str) % 2 == 0)
 	{
-		for (;i < strlen(s) ; i++)
+		for (;i < strlen(str) ; i++)
 		{
 			printf("%c", str[i]);
 		}
