bfc4fe7123af5f5a905573a8a8c9f8f28eb48622
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-02-28 14:16:18 -0800

    create a function that prints characters of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ *puts2 - prints other
+ *
+ *@str: variable as parameter for string
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		printf("%c\n", str[i]);
+		i += 1;
+	}
+}
