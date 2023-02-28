93441268490ef8af0d1d7ea9aaddd6765b7cd422
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-02-28 13:02:18 -0800

    creates a function which reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,14 +14,13 @@ void rev_string(char *s)
 	int index = 0;
 	char s1[strlen(s)];
 	int i = strlen(s) - 1;
+	long unsigned j = 0;
 
 	for(; i >= 0; i--)
 	{
 		s1[index] = s[i];
 		index += 1;
 	}
-
-	long j = 0;
 
 	for(; j < strlen(s1) - 1; j++)
 	{
