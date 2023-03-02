e1ccc720d006d75a86a857d5eccdb732ade03dd1
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-02 15:47:42 -0800

    add a funcion which prints letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,9 +10,9 @@ int main(void)
 	char lettr[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for( i = 0; lettr[i] != '\0'; i++)
+	for (i = 0; lettr[i] != '\0'; i++)
 	{
-		putchar( lettr[i]);
+		putchar(lettr[i]);
 	}
 	putchar('\n');
 	return (0);
