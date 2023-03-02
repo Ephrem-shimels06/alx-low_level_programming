b17354a913e0401298d41187bc699a82a9b5fa1c
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-02 14:18:15 -0800

    create a function which encode by rot13 principle

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a sentence
+ * @str: given string
+ * Return: string
+ */
+
+char *rot13(char *str)
+{
+	int i;
+	char storec[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char stores[] = "nopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+		{
+			s[i] = (s[i] - 65 > 25) ?
+				storel[s[i] - 97] : storeh[s[i] - 65];
+		}
+	}
+	return (s);
+}
+
