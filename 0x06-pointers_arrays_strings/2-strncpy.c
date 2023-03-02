d306645f3b4a630c1b8c67e45ca7edf4041019cd
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-02 00:38:35 -0800

    create a function that copy certain number of characters form one string to another

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ *_strncpy - copy specified number of characters form one string to another
+ *@dest: destination string pointer
+ *@src: source string pointer
+ *@n: interger which tells number of charcters to be copied
+ *Return: destination string pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int k = 0;
+	
+	for(;i < n && k < 98; i++)
+	{
+		dest[i] = src[i];
+		k++
+	}
+	return (dest);
+}
+
