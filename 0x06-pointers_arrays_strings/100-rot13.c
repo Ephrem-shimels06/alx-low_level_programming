ad161b644825601b2acfcbba378b56caf9810fbc
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-02 14:26:50 -0800

    create a function which encode by rot13 principle

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,8 +16,7 @@ char *rot13(char *str)
 	{
 		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
 		{
-			s[i] = (s[i] - 65 > 25) ?
-				stores[s[i] - 97] : storec[s[i] - 65];
+			s[i] = (s[i] - 65 > 25) ? stores[s[i] - 97] : storec[s[i] - 65];
 		}
 	}
 	return (s);
