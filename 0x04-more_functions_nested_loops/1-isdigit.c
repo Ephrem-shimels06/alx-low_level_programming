e0d6fc401e69a0d17665458908b3e0733661a5ae
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-03 11:07:29 -0800

    create files which answer tasks 1 upto 6 in 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include<stdio.h>
+
+/**
+ * _isdigit - checks whether a given digit is between 0 and 9
+ * @c: given digit
+ * Return: intger
+ */
+
 int _isdigit(int c)
 {
-	if (c >= 48 && c <=57)
+	if (c >= 48 && c <= 57)
 	{
 	return(1);
 	}
