444afd931cc4ea295be37f79edd7890c2a8b2e58
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-03 11:11:56 -0800

    modified 1-isdigit.c and 3-print_numbers.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * _isdigit - checks whether a given digit is between 0 and 9
@@ -11,8 +11,8 @@ int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 	{
-	return(1);
+	return (1);
 	}
-	return(0);
+	return (0);
 }
 
