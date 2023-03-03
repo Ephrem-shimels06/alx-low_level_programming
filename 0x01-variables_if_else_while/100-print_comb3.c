d96a2e322e737be1fe560bb8022a2c55e1a134bb
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-02 17:07:43 -0800

    add 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - print all possible combinations of two digits. 
- * Return: 0
+ * main - print all possible combinations of two digits.
+ *Return: 0
  */
 int main(void)
 {
