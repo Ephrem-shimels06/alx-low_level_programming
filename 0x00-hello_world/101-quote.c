af22447703939dd76083962774b825763683719f
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-04 04:01:27 -0800

    modify 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
+
 /**
 * main - It prints the sentence stored at a variable strr
-*
-* Retrun: retruns 1
+*Retrun: retruns 1
 */
- 
+
 int main(void)
 {
-char* strr = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char *strr = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 int i = 0;
-while( i < 59)
+while (i < 59)
 {
 putchar(strr[i]);
 i++;
 }
+putchar('\n');
 return (1); 
 }
