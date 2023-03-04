ffcb035cacbcab5d756497bb4a7278942e4e55e0
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-04 04:29:37 -0800

    modify 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 
-int main()
+/**
+ * main - using puts
+ * Return: always 0
+ */
+
+int main(void)
 
 {
-    char* test = "\"Programming is like building a multilingual puzzle";
-    puts(test);
-    return (0);
+char *test = "\"Programming is like building a multilingual puzzle";
+
+puts(test);
+return (0);
 }
