19076e4131be6fadff88263215c882f83bca9539
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-04 05:03:25 -0800

    modify 3-print_alphabets.c  to make it betty styled

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,23 +2,20 @@
 
 /**
  * main - prints lowecase and upper case letters
- *
  * Return: always 0
  */
-
 int main(void)
 {
-	int i;
-	
-	for ( i = 97; i < 123;i++)
-	{
-		putchar(i);
-	}
-	for (i = 65; i < 91; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
+int i;
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+for (i = 65; i < 91; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
 
