d013f2694f0d01a61452c24eab2402602270ac63
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-04 04:08:04 -0800

    modify 101-qiote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,11 +9,11 @@ int main(void)
 {
 char *strr = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 int i = 0;
-while (i < 59)
+while (i < 58)
 {
 putchar(strr[i]);
 i++;
 }
 putchar('\n');
-return (1); 
+return (1);
 }
