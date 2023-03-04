5a08560a9c720c503276ee87c6d305dfd500c457
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-04 10:03:00 -0800

    modify 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,21 +11,21 @@
 
 void puts_half(char *str)
 {
-        unsigned long i = strlen(str) / 2;
-        unsigned long j = (strlen(str) - 1) / 2 ;
-	if (strlen(str) % 2 == 0)
-	{
-		for (; i < strlen(str); i++)
-		{
-			printf("%c", str[i]);
-		}
-		printf("\n");
-	}else if (strlen(str) % 2 != 0)
-	{
-		for (; j < strlen(str); j++)
-		{
-			printf("%c", str[j]);
-		}
-		printf("\n");
-	}
+unsigned long i = strlen(str) / 2;
+unsigned long j = (strlen(str)) / 2 ;
+if (strlen(str) % 2 == 0)
+{
+for (; i < strlen(str); i++)
+{
+printf("%c", str[i]);
+}
+printf("\n");
+}else if (strlen(str) % 2 != 0)
+{
+for (j = j + 1; j < strlen(str); j++)
+{
+printf("%c", str[j]);
+}
+printf("\n");
+}
 }
