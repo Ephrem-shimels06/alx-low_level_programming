9bc37140bdb55610c40f40b8461cc5e3a35227b3
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-04 05:00:24 -0800

    modify 6-print_numberz.c  to make it betty styled

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,17 @@
 
 /**
  * main - print digits base 10
- *
  * Return: always 0
  */
 
 int main(void)
 {
-        int i;
+int i;
 
-        for (i = 48; i < 58; i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
-        return (0);
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
