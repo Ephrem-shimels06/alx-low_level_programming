1bba9a50ffe3a22d3b9a1ced730cf2463c853d30
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-04 04:53:41 -0800

    modify 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,12 +8,18 @@
 
 int main(void)
 {
-char hexa[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-	int i;
 
-	for (i = 0; i < 16; i++)
+	int i;
+	int j;
+
+	for (j = 48; j < 58;j++)
 	{
-		putchar(hexa[i]);
+		putchar(j);
+	}
+
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
