1aaf9e7a7a9083627a3d566baf1a5768edcec313
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-06 12:07:10 -0800

    create solution for the first task and RADME and header file for the directory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: a string where a new byte place on
+ * @b: a new which going to be put on 's'
+ * Return: a pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
