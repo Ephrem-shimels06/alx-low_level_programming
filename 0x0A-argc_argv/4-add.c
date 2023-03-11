87d1046cf91b274d499d4d73bc91e9cc8f2f278d
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-11 01:18:13 -0800

    modify 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 0; i < argc; i++)
 		{
-			if (*argv[i] >= 48 || *argv[i] <= 57)
+			if (*argv[i] >= 0 || *argv[i] <= 10)
 			{
 				sum += *argv[i];
 			}
