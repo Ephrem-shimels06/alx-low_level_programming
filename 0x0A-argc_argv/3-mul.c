897f4269ce5d04292567d972dd55dbc2dc667e3d
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-11 01:32:34 -0800

    modify to correct the output

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the multiplication result of two numbers
@@ -12,11 +13,12 @@ int main(int argc, char *argv)
 {
 	if (argc > 1)
 	{
-		printf("%d\n", argv[1] * argv[2]);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	else
 	{
 		printf("Error\n");
+		return (1);
 	}
 
 	return (1);
