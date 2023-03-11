a8625166d646d593ecff32d1e097d9de5e7bf244
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-11 01:15:18 -0800

    modify 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - adds the integer arguments of a program
+ * @argc: number of arguments
+ * @argv: array of argments
+ * Return: always 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (*argv[i] >= 48 || *argv[i] <= 57)
+			{
+				sum += *argv[i];
+			}
+			else
+			{
+				printf("Error\n");
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (1);
+}
