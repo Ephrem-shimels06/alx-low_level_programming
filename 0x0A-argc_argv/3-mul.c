5290fe63bfeaac7cd3418be637ade14f93fb8b34
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-11 00:38:14 -0800

    create a program which prints the multiplication relut of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the multiplication result of two numbers
+ * @argc:  number of arguments
+ * @argv: array of arguments
+ * Return: always 1
+ */
+
+int main(int argc, char *argv)
+{
+	if (argc > 1)
+	{
+		printf("%d\n", argv[1] * argv[2]);
+	}
+	printf("Error");
+	return (1);
+}
