399c2a57d03fa595203d828274d0eab38cebd549
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-11 00:06:25 -0800

    add space after comma at line 11 and 13

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - print the number of arguments passed into it
@@ -7,9 +8,9 @@
  * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%d\n",argc);
+	printf("%d\n", argc);
 
 	return (0);
 }
