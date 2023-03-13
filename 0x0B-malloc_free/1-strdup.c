352627b71614b36657c573cc715b52508d9edd70
Ephrem-shimels06 <ephrem21shimels@gmail.com>
2023-03-13 03:13:43 -0700

    create a c file 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * @str: the given string
+ * Return: a pointer to new allocated space
+ */
+
+char *_strdup(char *str)
+{
+	char *cpy;
+
+	cpy = malloc(strlen(str));
+	cpy = str;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (cpy);
+	}
+}
